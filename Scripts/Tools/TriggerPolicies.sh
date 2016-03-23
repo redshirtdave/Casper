5a1c46fc7aab17188ece3126df0da5e5b4310bad
mondada <dcmccracken@MacBook-Pro-7831c1cfe9c6.local>
2016-03-23 14:59:51 +1100

    Re-Structured scripts directory
    Renamed all EA script folders
    Added Tools directory for other useful stuff

---
+++ Scripts/Tools/TriggerPolicies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Split all supplied Parameters into an array
+Parameters=( "$@" )
+
+# Begin cycling through the Parameters array from index 3 and execute each trigger
+i=3
+while [ ${i} -lt ${#Parameters[@]} ]
+do
+    if [ -n "${{Parameters[i]}" ]
+    then
+    	jamf policy -trigger "${Parameters[i]}"
+    fi
+	let i++
+done
+
+exit 0
